2b43d90627704fcfc8ce7b33c50f706bcaf23879
Zeeshan <zeeshanalimyamu@gmail.com>
2023-02-28 12:26:34 +0530

    Movie rating website by ReactJS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { element } from 'prop-types';
 import './App.css';
-
+import Header from './components/Header';
+import Movie from './components/Movie';
+import movie from  './movie.json';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+           <Header/>
+        <div className="main">
+          {
+           movie.map((element,index)=> {
+              return(
+                <Movie
+                 key= {index} 
+                 Title={element.Title}
+                 year={element.Year}
+                 Img={element.Poster}
+                 />  
+
+              )
+            })
+
+          }
+           
+
+        </div>
     </div>
   );
 }
